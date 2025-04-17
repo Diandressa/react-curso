3b60fea10b811341a049195040bb5a58c804ebc9
Andressa Nicolau <andressa_nicolau@hotmail.com>
2025-04-17 17:22:44 -0300

    criando um componente

---
+++ src/components/Topo.tsx
@@ -0,0 +1,12 @@
+export default function Topo() {
+    return(
+        <div className='flex justify-between items-center bg-cyan-400 h-[100px] '>
+            <div>Logo</div>
+            <div className="flex flex-col justify-center items-center">
+                <div className="text-3xl">CFBCursos</div>
+                <div className="subtituloTopo">Curso de React</div>
+            </div>
+            <div>Andressa</div>
+        </div>
+    )
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,30 +1,11 @@
 //import Image from "next/image";
 //import { Geist, Geist_Mono } from "next/font/google";
 
-const nome='Bruno Nicolau';
-const nomeFuncao='Andressa';
-let canal = 'CFB Cursos';
+import Topo from "@/components/Topo";
 
-// function retnome(){
-//   return nomeFuncao
-// }
-// const cnl = ()=> {
-//   return canal
-// }
-
-//usamos o className para atribuir uma classe à tag
-function Topo(){
-  return(
-    <div className='flex justify-between items-center bg-cyan-400 h-[100px] '>
-      <div>Logo</div>
-      <div className="flex flex-col justify-center items-center">
-        <div className="text-3xl">{canal}</div>
-        <div className="subtituloTopo">Curso de React</div>
-      </div>
-      <div>{nome}</div>
-    </div>
-  ) 
-}
+// const nome='Bruno Nicolau';
+// const nomeFuncao='Andressa';
+// let canal = 'CFB Cursos';
 
 export default function Home() {
   return (
