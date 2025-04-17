7fce09b06c8e0f8b0861e211f46b2e8b103b6ad3
Andressa Nicolau <andressa@ceert.org.br>
2025-04-17 14:22:09 -0300

    funcão que retorna component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,34 +1,35 @@
 //import Image from "next/image";
 //import { Geist, Geist_Mono } from "next/font/google";
 
-// const nome='Bruno Nicolau';
-// const nomeFuncao='Andressa';
-// let canal = 'CFB Cursos';
+const nome='Bruno Nicolau';
+const nomeFuncao='Andressa';
+let canal = 'CFB Cursos';
 
-//usar o retorno de uma função
-function retnome(){
-  return nomeFuncao
+// function retnome(){
+//   return nomeFuncao
+// }
+// const cnl = ()=> {
+//   return canal
+// }
+
+//função que retorna um bloco mais complexo(com um componente) usamo return() com parenteses
+function Topo(){
+  return(
+    <div>
+      <div>Logo</div>
+      <div>{canal}</div>
+      <div>{nome}</div>
+    </div>
+  ) 
 }
 
 export default function Home() {
-  //escopo da função agora, só é acessada dentro dessa função
-  const nome='Bruno Nicolau';
-  const nomeFuncao='Andressa';
-  let canal = 'CFB Cursos';
-  //var canal = 'CFB Cursos'; tb dá erro por causa do typescript, ele não permite pq usa o Strict Mode
-
-  //posso usar a função dentro da função home, ela acessa as variáveis pq as variáveis estão no escopo global
-  // function retnome(){
-  //   return nomeFuncao
-  // }
-
-  //return tem quer a última coisa da função
   return (
       <div>
-        <div>CFB React</div>
-        <div>{canal}</div>
-        <div>{nome}</div>
-        <div>{retnome()}</div>
+        {/* utilizar a função Topo */}
+        {/*Topo()*/}
+        {/* podemos utilizar como uma tag <> pq ela retorna um componente, o nome da função precisa estar com a primeira letra maiúscula, para o react entender que é um componente */}
+        <Topo/>
       </div>      
   );
 }
