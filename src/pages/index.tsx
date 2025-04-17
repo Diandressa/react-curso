5e3939fdf11708223b2e47ed7ca6d270cf69ecf9
Andressa Nicolau <andressa_nicolau@hotmail.com>
2025-04-17 17:06:58 -0300

    style inline e style inline como objeto - js

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -30,6 +30,22 @@ export default function Home() {
   return (
       <div>
         <Topo/>
+        {/*style inline css com objeto, usamos uma chave só pq a outra chave já está no objeto*/}
+        <div style={cssobjeto}>
+          {/*style inline, usa css do javascript. Segui a sintaxe do css no js*/}
+          <div style={{color:'#fff', backgroundColor: 'darkcyan', marginRight:'1rem', paddingInline:'1rem'}}>Curso de React Next</div>
+          <div>Typescript</div>
+        </div>
       </div>      
   );
+}
+
+//Style inline css com objeto
+const cssobjeto={
+  display:'flex',
+  justifyContent:'center',
+  alignItems:'center',
+  color:'#aaa',
+  backgroundColor:'#eee',
+  fontSize:'20px',
 }
