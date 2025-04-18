47cb23dd7c5066586c2aaed5e832a90717c1d5bc
Andressa Nicolau <andressa_nicolau@hotmail.com>
2025-04-18 18:45:21 -0300

    renderizando componentes com array de objetos

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -5,36 +5,14 @@ interface CardProps{
     produto:string,
     valor:number,
     desconto:number,
-    //funcao:()=>void
     funcao:any
 }
 
-//Tem duas tipos de operações condicionais ternárias: ternária normal e o resumo da ternária
-//ternário: usamos quanto houver condição verdadeira e falsa
-//resumo ternário: só quando tiver a condição verdadeira
-
-//Podemos usar a operação condicional nos estilos tb
-//transformamos o estilo em script pra usar o operador ternário
-//*Quando tiver desconto quero que a borda fica azul e quando não tiver ela fica vermelha
-
 export default function Card(props:CardProps){
     return(
-        <div className={`flex flex-col border-4 ${props.desconto > 0 ? 'border-b-fuchsia-400' : '  border-cyan-400'} p-4 my-6 rounded`}>
+        <div className={`flex flex-col justify-center border-4 ${props.desconto > 0 ? 'border-b-fuchsia-400' : '  border-cyan-400'} p-4 my-6 rounded`}>
             <div>Produto:{props.produto}</div>
             <div>Valor: R${props.valor}</div>
-            
-            {/* chamar função se o desconto for maior que zero. Usamos a operação condicional ternária*/}
-            {/* 
-            {props.desconto > 0 ? (
-                //Chama essa função que renderiza os elementos desejados
-                <div>
-                    <div>Desconto: R${props.desconto}</div>
-                    <div>Preço Venda: R${props.funcao(props.valor, props.desconto)}</div>
-                </div>
-            ) : <div>Sem desconto</div>}
-            */}
-
-            {/* como essa operação só retorna algo se for true então podemos usar o resumo da operação ternária, ele não retorna nada se for false*/}
 
             {props.desconto > 0 && (
                 <div>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,10 +4,51 @@
 import Topo from "@/components/Topo";
 import Card from "@/components/Card";
 
-//para passar uma função o componente pai, precisamos passar para o pai e depois o filho herda
-//não podemos passar a função para o pai por meio do filho, podemos passar a função pai para o componente filho
+//array de objetos
+const produtos=[
+  {
+    produto: 'Mouse',
+    valor: 49.9,
+    desconto: 0,
+    disponivel: true
 
-//funcao que faz o calculo, ele tem que receber os parametros do Card. O card chama essa função, e mandamos os parâmetro valor e desconto lá no Card.tsx
+  },
+  {
+    produto: 'Teclado',
+    valor: 659.9,
+    desconto: 10,
+    disponivel: true
+
+  },
+  {
+    produto: 'Monitor',
+    valor: 689.9,
+    desconto: 0,
+    disponivel: false
+
+  },
+  {
+    produto: 'CPU',
+    valor: 829.9,
+    desconto: 100,
+    disponivel: true
+
+  },
+  {
+    produto: 'Caixa Som',
+    valor: 39.9,
+    desconto: 0,
+    disponivel: true
+
+  },
+  {
+    produto: 'Microfone',
+    valor: 65.0,
+    desconto: 0,
+    disponivel: true
+
+  }
+]
 
 //esse função pode ser chamada em qualquer componente, precisamos definir o tipo dele na interface do componente somente
 function calcDesc(valor:number, desconto:number){
@@ -22,27 +63,21 @@ export default function Home() {
   return (
       <div>
         <Topo/>
-        <div style={cssobjeto}>
-          <div style={{color:'#fff', backgroundColor: 'darkcyan', marginRight:'1rem', paddingInline:'1rem'}}>Curso de React Next</div>
-          <div>Typescript</div>
+        <div>
+          <div style={{color:'#fff', backgroundColor: 'darkcyan', textAlign: 'center'}}>Curso de React Next</div>
         </div>
-        <div className="flex justify-center gap-3">
-          {/* passando a propriedade que era receber na função desse componente - armazena na variável props*/}
-          <Card produto={"Mouse"} valor={49.90} desconto={10} funcao={calcDesc}/>
-          <Card produto={"Teclado"} valor={69.90} desconto={0} funcao={calcDesc}/>
-          <Card produto={"Monitor"} valor={459.90} desconto={0} funcao={calcDesc2}/>
-          <Card produto={"CPU"} valor={799.90} desconto={50} funcao={calcDesc2}/>
+        <div className="flex flex-wrap justify-center gap-3">
+          {/*Entra com js usando as {}. Retorna o Card para cada elemento do array */}
+          {
+            produtos.map((e:any)=>{
+              if(e.disponivel){
+                return (
+                  <Card produto={e.produto} valor={e.valor} desconto={e.desconto} funcao={calcDesc}/>
+                )
+              }
+            })
+          }
         </div>
       </div>      
   );
-}
-
-//Style inline css com objeto
-const cssobjeto={
-  display:'flex',
-  justifyContent:'center',
-  alignItems:'center',
-  color:'#aaa',
-  backgroundColor:'#eee',
-  fontSize:'20px',
 }
