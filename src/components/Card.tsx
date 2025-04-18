b0a4f95fdf2f1fc4ebb653baa458dacdfd4b1be5
Andressa Nicolau <andressa_nicolau@hotmail.com>
2025-04-18 16:23:23 -0300

    passando função para o componente

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -5,15 +5,28 @@ interface CardProps{
     produto:string,
     valor:number,
     desconto:number,
+    //funcao:()=>void
+    funcao:any
 }
+
+//podemos criar uma função de calcular aqui no Card e depois passar para o componente
+//porém criamos em index e passamos o valor calculado para o componente já
+//essa funçao só pode ser usada nesse componente
+function promocao(valor:number, desconto:number):number{
+    return valor + desconto;
+}
+
 export default function Card(props:CardProps){
     return(
         <div className="flex flex-col border border-b-fuchsia-400 p-4 my-6 rounded">
             <div>Produto:{props.produto}</div>
             <div>Valor: R${props.valor}</div>
             <div>Desconto: R${props.desconto}</div>
-            {/* operação com props*/}
-            <div>Preço Venda: R${props.valor - props.desconto}</div>
+            {/* chamar função e passar os parâmetro que ela aguarda*/}
+            <div>Preço Venda: R${props.funcao(props.valor, props.desconto)}</div>
+
+            {/* chamar função criada dentro de card*/}
+            <div>Promoção: R${promocao(props.valor, props.desconto)}</div>
         </div>   
     )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,6 +4,20 @@
 import Topo from "@/components/Topo";
 import Card from "@/components/Card";
 
+//para passar uma função o componente pai, precisamos passar para o pai e depois o filho herda
+//não podemos passar a função para o pai por meio do filho, podemos passar a função pai para o componente filho
+
+//funcao que faz o calculo, ele tem que receber os parametros do Card. O card chama essa função, e mandamos os parâmetro valor e desconto lá no Card.tsx
+
+//esse função pode ser chamada em qualquer componente, precisamos definir o tipo dele na interface do componente somente
+function calcDesc(valor:number, desconto:number){
+  return valor - desconto;
+}
+
+function calcDesc2(valor:number, desconto:number){
+  return valor - (desconto/2)
+}
+
 export default function Home() {
   return (
       <div>
@@ -14,10 +28,10 @@ export default function Home() {
         </div>
         <div className="flex justify-center gap-3">
           {/* passando a propriedade que era receber na função desse componente - armazena na variável props*/}
-          <Card produto={"Mouse"} valor={49.90} desconto={10}/>
-          <Card produto={"Teclado"} valor={69.90} desconto={2}/>
-          <Card produto={"Monitor"} valor={459.90} desconto={5}/>
-          <Card produto={"CPU"} valor={799.90} desconto={50}/>
+          <Card produto={"Mouse"} valor={49.90} desconto={10} funcao={calcDesc}/>
+          <Card produto={"Teclado"} valor={69.90} desconto={2} funcao={calcDesc}/>
+          <Card produto={"Monitor"} valor={459.90} desconto={0} funcao={calcDesc2}/>
+          <Card produto={"CPU"} valor={799.90} desconto={50} funcao={calcDesc2}/>
         </div>
       </div>      
   );
