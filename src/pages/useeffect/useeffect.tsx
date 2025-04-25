1e499ceb758948035f04edb548d51fd4cc8c9ff7
Andressa Nicolau <andressa_nicolau@hotmail.com>
2025-04-25 10:53:32 -0300

    useEffect ao chamar a funçao ou uma vez somente

--- src/pages/useeffect/useeffect.tsx
+++ src/pages/useeffect/useeffect.tsx
@@ -7,28 +7,41 @@ import { useState, useEffect } from "react";
 export default function UseEffect(){
     //espera uma funçao de callback, quando ela é acionado o useEffect acontece, pode ser até a função useState. Quando use state é acionado.
     const [cont, setCont] = useState<number>(0);
-    let aux=0;
-
-    function add(){
-        let c = cont;
-        c++
-        setCont(c)
-    }
+    //criar um state aux
+    const [aux, setAux] = useState<number>(0);
 
     useEffect(()=>{
         //o useEffect é acionado quando acontece renderização, e a renderização está sendo chamada no setCont que renderiza com zero inicialmente. Isso é a renderização continua
 
-        let c = cont;
-        c++
-        setCont(c)
+        // let c = cont;
+        // c++
+        // setCont(c)
 
         //variável comum não aciona a renderização da página, só quando uso o useState
         //o valor de aux é 1 agora, mas não é renderizado na tela
-        aux++
+        // aux++
 
         //chamar useEffect uma vez, somente quando a página for carregada ao abrir a página. Pra isso usamos o segundo parâmetros, que são chamadas de dependências. A segunda dependência é um array vazio.
         //o array vazio chama o effect uma vez 
-    },[])
+
+        //chamar useEffect somente quando o useState for alterado, sem looping.
+
+        //O use effect é disparado pq quando abrimos a página ele renderiza o aux. E ele executa só nessa renderização, por causa do colchetes vazio []
+        alert('Use effect disparado')
+
+        //se eu passo o aux na dependência, sempre que aux é alterado e a página é renderizada ele chama o alert
+    },[aux])
+
+    function add(){
+        // let c = cont;
+        // c++
+        // setCont(c)
+
+        //alterar o aux ao clicar em add
+        let a = aux;
+        a++;
+        setAux(a);
+    }
 
     return(
         <div>
