d3aed8752c5c2bddfb842506606b027ade51def5
Andressa Nicolau <andressa_nicolau@hotmail.com>
2025-04-30 12:13:09 -0300

    consumindo api digimon com fetch

--- src/pages/filtragem/filtragem.tsx
+++ src/pages/filtragem/filtragem.tsx
@@ -1,59 +1,74 @@
 import Topo from "@/components/Topo";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
-const carros=[
-    {id:0, categ:'Esporte', valor:'120000.00', modelo: 'Golf GTI'},
-    {id:1, categ:'Esporte', valor: '200000.00', modelo: 'Camaro'},
-    {id:2, categ:'SUV', valor: '100000.00', modelo: 'HRV'},
-    {id:3, categ: 'SUV', valor: '100000.00', modelo: 'T-Cross'},
-    {id:4, categ: 'Utilitario', valor: '180000.00', modelo: 'Hillux'},
-    {id:5, categ: 'Utilitario', valor: '160000.00', modelo: 'Ranger'},
-]
+let digimons:any = ''
+let listaPronta = false;
 
 export default function Filtragem(){
-    const [categ, setCateg] = useState<string>('');
+    const [level, setLevel] = useState<string>('');
     const [linhas, setLinhas] = useState<any[]>([]);
 
-    function criarLinhas(cat:any){
-        //coloca a categoria selecionada no select, que foi enviada para a função no clique, no useState
-        setCateg(cat);
-
-        //percorrer o array e retornar os elementos que correspondem a categoria selecionada
-
-        let linha:any[] = []
-        
-        carros.forEach((carro:any)=>{
-            //se o item tiver a mesma string da que foi passada pra função eu coloca na lista linha o conteúdo que quero renderizar com os valores daquele item
-            if(carro.categ == cat){
-                linha.push(
-                <div className="flex flex-row w-[500px]" key={carro.id}>
-                    <div className="w-full">{carro.categ}</div>
-                    <div className="w-full">{carro.valor}</div>
-                    <div className="w-full">{carro.modelo}</div>
-                </div>
-                )
-            }
+    //consumir api com useEffect, renderiza somente uma vez ao abrir a página. Manda os dados para lista
+    useEffect(()=> {
+        fetch('https://digimon-api.vercel.app/api/digimon')
+        .then(response=>response.json())
+        .then(response=>{
+            //passa o resultado da api para variável digimon
+            digimons = response
+            listaPronta = true
         })
+    }, [])
 
-        //seta a nova array no setLinhas, para renderizar na tela
-        setLinhas(linha)
+    console.log(digimons)
+
+    function criarLinhas(levelSelecionado:any){
+        if(listaPronta){
+            //coloca a categoria selecionada no select, que foi enviada para a função no clique, no useState
+            setLevel(levelSelecionado);
+
+            //percorrer o array e retornar os elementos que correspondem a categoria selecionada
+
+            let linha:any[] = []
+            
+            digimons.forEach((digimon:any)=>{
+                //se o item tiver a mesma string da que foi passada pra função eu coloca na lista linha o conteúdo que quero renderizar com os valores daquele item
+                if(digimon.level == levelSelecionado){
+                    linha.push(
+                    <div className="flex flex-row w-[500px]" key={digimon.name}>
+                        <div className="w-full">{digimon.name}</div>
+                        <div className="w-full">{digimon.level}</div>
+                        <div className="w-full"><img src={digimon.img}/></div>
+                    </div>
+                    )
+                }
+            })
+
+            //seta a nova array no setLinhas, para renderizar na tela
+            setLinhas(linha)
+        } else {
+            alert('Lista ainda não foi carregada')
+        }
+        
     }
 
     return(
         <>
             <Topo/>
             <label>Selecione a categoria</label>
-            <select value={categ} onChange={(e)=>{criarLinhas(e.target.value)}}>
-                <option value="">Nenhuma</option>
-                <option value="Esporte">Esporte</option>
-                <option value="SUV">SUV</option>
+            <select value={level} onChange={(e)=>{criarLinhas(e.target.value)}}>
+                <option value="In Training">In Training</option>
+                <option value="Rookie">Rookie</option>
+                <option value="Champion">Champion</option>
+                <option value="Fresh">Fresh</option>
+                <option value="Ultimate">Ultimate</option>
+                <option value="Mega">Mega</option>
             </select>
 
             <div className="flex flex-col">
                 <div className="flex flex-row w-[500px]">
-                    <div className="w-full">Categoria</div>
-                    <div className="w-full">Valor</div>
-                    <div className="w-full">Modelo</div>
+                    <div className="w-full">Nome</div>
+                    <div className="w-full">Level</div>
+                    <div className="w-full">Imagem</div>
                 </div>
                 {linhas}
             </div>
